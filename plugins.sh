4d79436300d47824d6e265af10a18c6d8713ce99
Joakim Bergman <joab@kth.se>
2012-08-27 11:40:19 +0200

    Stupid simple script for plugins

--- plugins.sh
+++ plugins.sh
@@ -1,13 +1,20 @@
-https://github.com/altercation/vim-colors-solarized.git
-https://github.com/godlygeek/tabular.git
-https://github.com/kien/ctrlp.vim.git
-https://github.com/majutsushi/tagbar.git
-https://github.com/mileszs/ack.vim.git
-https://github.com/nvie/vim-flake8.git
-https://github.com/scrooloose/syntastic.git
-https://github.com/tpope/vim-commentary.git
-https://github.com/tpope/vim-fugitive.git
-https://github.com/tpope/vim-repeat.git
-https://github.com/tpope/vim-surround.git
-https://github.com/tpope/vim-unimpaired.git
-https://github.com/xolox/vim-easytags.git
+#!/bin/sh
+
+mkdir -p .vim/bundle
+cd .vim/bundle
+
+git clone https://github.com/altercation/vim-colors-solarized.git
+git clone https://github.com/godlygeek/tabular.git
+git clone https://github.com/kien/ctrlp.vim.git
+git clone https://github.com/majutsushi/tagbar.git
+git clone https://github.com/mileszs/ack.vim.git
+git clone https://github.com/nvie/vim-flake8.git
+git clone https://github.com/scrooloose/syntastic.git
+git clone https://github.com/tpope/vim-commentary.git
+git clone https://github.com/tpope/vim-fugitive.git
+git clone https://github.com/tpope/vim-repeat.git
+git clone https://github.com/tpope/vim-surround.git
+git clone https://github.com/tpope/vim-unimpaired.git
+git clone https://github.com/xolox/vim-easytags.git
+
+cd -
